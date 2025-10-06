ce3aa8a27f0cdb548210ea3925db99925347295b
Samuel Ndegwa <samuel.ndegwa@student.strathmore.edu>
2025-10-06 20:27:25 +0300

    Fix Next.js font loading warning by using next/font/google instead of next/head

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,22 +1,22 @@
 import '../styles/globals.css'
-import Head from 'next/head'
+import { Poppins } from 'next/font/google'
 import { AuthProvider } from '../lib/AuthContext'
 import Header from '../components/layout/Header'
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+})
+
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+    <div className={poppins.className}>
       <AuthProvider>
         <Header />
         <Component {...pageProps} />
       </AuthProvider>
-    </>
+    </div>
   )
 }
 
